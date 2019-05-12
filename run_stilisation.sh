859d8376f678d3437df5d558dbc11b66f97f5afb
Corentin Chauvin-Hameau <corentin.chauvin@gmail.com>
2019-05-12 21:56:45 +0200

    Add distillation from ChristopherSweeney/SlimNets (commit 74ec7bc)

---
+++ run_stilisation.sh
@@ -0,0 +1,5 @@
+python neural_style/neural_style.py eval \
+  --content-image images/content-images/amber.jpg \
+  --model saved_models/mosaic.pth \
+  --output-image images/output-images/test.jpg \
+  --cuda 0
